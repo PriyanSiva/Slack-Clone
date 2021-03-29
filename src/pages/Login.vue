f8e186fe0e45648a078017ff14ab49997c24c58c
PriyanSiva <priyansiva1995@gmail.com>
2021-03-29 07:39:43 +0530

    initial commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+       <div class="jumbotron bg-primary text-white text-center py-5">
+           <h2 class="lead display-3">#SLACK#</h2>
+           <p>Realtime communication at it's best!</p>
+       </div>
+
+       <div class="container-fluid text-center">
+           <div class="row mt-5">
+               <div class="col">
+                   <button class="btn btn-outline-danger btn-lg">Login with Google</button>
+               </div>
+           </div>
+            <div class="row mt-5">
+               <div class="col">
+                   <button class="btn btn-outline-info btn-lg">Login with Twitter</button>
+               </div>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
+
+<style scoped>
+.btn, .jumbotron {
+    border-radius: 0px;
+}
+</style>
